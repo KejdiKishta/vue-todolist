0a9fc7a9d86bba2d9262d88f0884ad3360894184
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-20 16:55:29 +0200

    add: milestone 2

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@
 // MILESTONE 1
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-
-//*DA FARE
 // MILESTONE 2
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+//*DA FARE
 // Bonus:
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista (cercate nella documentazione l'evento che vi serve per questo bonus)
 // 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente ed eventuale icona (se done era uguale a false, impostare true e viceversa)
@@ -40,11 +40,21 @@ createApp({
                     text: "Summit update to mobile storefronts",
                     done: true,
                 },
-            ]
+            ],
 
+            newTask: {
+                text: "",
+                done: false,
+            },
         }
     },
-    methods() {
-
+    methods: {
+        addTask: function () {
+            //copia di newTask
+            const taskCopy = { ...this.newTask };
+            console.log(taskCopy);
+            this.list.push(taskCopy);
+            this.newTask.text = "";
+        }
     }
 }).mount("#app")
